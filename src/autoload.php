bb2893fa2f5864dba03ff23476a3ac8dac622747
⚡ Sigui <siguici@proton.me>
2025-02-12 21:54:26 +0000

    🔗 Auto-synced from phi-x/phi@2ab3a0eb147d3003fa1a6e5add98081460c3a45b: ✨ Use Phi's autoloader as default

---
+++ src/autoload.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Phi;
+
+class Autoloader
+{
+    public function register(): static
+    {
+        spl_autoload_register($this->autoload(...));
+
+        return $this;
+    }
+
+    public function unregister(): static
+    {
+        spl_autoload_register($this->autoload(...));
+
+        return $this;
+    }
+
+    public function autoload(string $class): bool
+    {
+        $exts = $this->autoloadExtensions();
+
+        if (str_starts_with($class, __NAMESPACE__.'\\')) {
+            return $this->loadClass($class, $exts);
+        }
+
+        return false;
+    }
+
+    public function loadClass(string $class, string $exts): bool
+    {
+        $name = substr($class, strlen(__NAMESPACE__.'\\'));
+
+        foreach (explode(',', $exts) as $ext) {
+            if ($this->loadFile($name, $ext)) {
+                if (! class_exists($class) && ! interface_exists($class) && ! trait_exists($class) && ! enum_exists($class)) {
+                    throw new \RuntimeException("$class not found.");
+
+                    return false;
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function loadFile(string $name, string $ext = '.php'): bool
+    {
+        if (is_file($file = __DIR__.DIRECTORY_SEPARATOR.str_replace(['\\', '/'], DIRECTORY_SEPARATOR, $name).$ext)) {
+            include_once $file;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public function autoloadExtensions(): string
+    {
+        $exts = explode(',', spl_autoload_extensions());
+
+        $exts[] = '.phi';
+
+        return spl_autoload_extensions(implode(',', $exts));
+    }
+}
+
+$phiLoader = (new Autoloader)->register();
+
+$phiLoader->loadFile('phi');
+
+return $phiLoader;
