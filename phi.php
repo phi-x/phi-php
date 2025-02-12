e7b912f5d3510e5adc98c5a48c8735e56b8f1006
⚡ Sigui <siguici@proton.me>
2025-02-12 16:40:22 +0000

    🔗 Auto-synced from phi-x/phi@0d36e4d983b025492e99168ef993148b09e56b49: ✨ Add the Phi (Φ) binary

---
+++ phi.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php
+<?php
+
+$bin = realpath(__DIR__.'/bin/phi');
+
+$contents = file_get_contents($bin);
+$contents = preg_replace('{^#!/.+\r?\n<\?(php)?}', '', $contents, 1, $replaced);
+
+if ($replaced) {
+    $contents = strtr($contents, [
+        '__FILE__' => var_export($bin, true),
+        '__DIR__' => var_export(dirname($bin), true),
+    ]);
+    eval($contents);
+    exit(0);
+}
+
+include $bin;
+exit;
