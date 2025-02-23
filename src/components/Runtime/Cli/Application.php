44b332f0f09845d2583e8aae16b0ae2683c8083a
⚡ Sigui <siguici@proton.me>
2025-02-23 22:14:35 +0000

    🔗 Auto-synced from phi-x/phi@63981d240b9716474112794767bad299f47441fb: 🎉 Begin the Phi Runtime application

---
+++ src/components/Runtime/Cli/Application.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phi\Runtime\Cli;
+
+use Phi\Foundation\Cli\Application as BaseApplication;
+
+class Application extends BaseApplication {}
---
+++ src/components/Runtime/Http/Application.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phi\Runtime\Http;
+
+use Phi\Foundation\Http\Application as BaseApplication;
+
+class Application extends BaseApplication {}
